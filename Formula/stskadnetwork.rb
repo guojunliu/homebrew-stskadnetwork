a05a84651bb47dcfb35360d4bd6b486e8e60b215
steve <909476821@qq.com>
2021-06-30 19:12:54 +0800

    add 101

--- Formula/stskadnetwork.rb
+++ Formula/stskadnetwork.rb
@@ -4,7 +4,7 @@
 class Stskadnetwork < Formula
   desc "合并各广告平台SKAdNetwork工具"
   homepage "https://github.com/guojunliu/stskadnetwork"
-  url "https://github.com/guojunliu/stskadnetwork/blob/master/product/1.0.1/stskadnetwork_1.0.1.tar.gz"
+  url "https://github.com/guojunliu/stskadnetwork/blob/master/product/101/stskadnetwork_1.0.1.tar.gz"
   version "1.0.1"
   sha256 "186180f27be2078055c5946f9403596e5f0e3b925502c90e91800310bcffc49f"
   license "MIT"
