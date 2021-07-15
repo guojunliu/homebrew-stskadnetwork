6a174cdc07e85be50284b57d183a2574712f578c
steve <909476821@qq.com>
2021-07-15 16:51:25 +0800

    2.0.0

--- Formula/stskadnetwork.rb
+++ Formula/stskadnetwork.rb
@@ -4,9 +4,9 @@
 class Stskadnetwork < Formula
   desc "合并各广告平台SKAdNetwork工具"
   homepage "https://github.com/guojunliu/stskadnetwork"
-  url "https://github.com/guojunliu/stskadnetwork/raw/master/product/1.0.2/stskadnetwork_1.0.2.tar.gz"
-  version "1.0.2"
-  sha256 "07fbaed53638f560d0ce48d64b98b0af53fe2d74baee646c4b9705995c3a4aa3"
+  url "https://github.com/guojunliu/stskadnetwork/raw/master/product/2.0.0/stskadnetwork_2.0.0.tar.gz"
+  version "2.0.0"
+  sha256 "69813a53d6d794d2373de6527940606172cad2fee5680eaa1ad4692b3f0aafca"
   license "MIT"
 
   def install
